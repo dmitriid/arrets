03196edd47e514324aa9a0003af11a307a41f778
Dmitrii Dimandt <dmitrii@dmitriid.com>
2016-01-03 21:35:03 +0100

    Add Apache License version 2.0

--- src/arrets.erl
+++ src/arrets.erl
@@ -1,10 +1,22 @@
 %%%-----------------------------------------------------------------------------
-%%% @author dmitriid
-%%% @copyright (C) 2015
-%%% @doc
+%%% Copyright 2016 Dmitrii Dimandt <dmitrii@dmitriid.com>
 %%%
-%%% @end
-%%% Created : 30. Dec 2015 11:50
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%-----------------------------------------------------------------------------
+
+%%%-----------------------------------------------------------------------------
+%%% @doc A silly non-performant array implementation on top of ets.
+%%%      See README for documentation
 %%%-----------------------------------------------------------------------------
 -module(arrets).
 
